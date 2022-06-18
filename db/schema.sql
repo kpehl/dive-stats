5491add0817a1ce75c36a23f9924777a42739689
Kathleen Pehl <kathleen.pehl@gmail.com>
2022-06-18 14:15:48 -0400

    created tables for tags, locations, and dives; created random_between function in schema and used it to generate random data for locations and dives to seed database

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,10 @@ CREATE DATABASE diving_db;
 
 CREATE DOMAIN UNSIGNED AS INTEGER CHECK (VALUE > 0);
 
+CREATE DOMAIN LATLONG AS POINT
+    CHECK (VALUE[0] BETWEEN -90 AND 90)
+    CHECK (VALUE[1] BETWEEN -180 AND 180);
+
 CREATE TABLE certifications (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -20,3 +24,32 @@ CREATE TABLE divers (
     is_instructor BOOLEAN DEFAULT FALSE NOT NULL,
     certification_id INTEGER REFERENCES certifications(id) ON DELETE SET NULL
 );
+
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    coordinates LATLONG NOT NULL
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    location_id INTEGER NOT NULL REFERENCES locations(id) ON DELETE CASCADE,
+    UNIQUE(name, location_id)
+);
+
+CREATE TABLE dives (
+    id SERIAL PRIMARY KEY,
+    depth NUMERIC(5,2) NOT NULL,
+    dive_date TIMESTAMP NOT NULL DEFAULT NOW(),
+    duration UNSIGNED NOT NULL,
+    diver_id INTEGER NOT NULL REFERENCES divers(id) ON DELETE CASCADE,
+    location_id INTEGER NOT NULL REFERENCES locations(id) ON DELETE CASCADE
+);
+
+CREATE FUNCTION random_between(low INT, high INT)
+RETURNS INT AS $$
+BEGIN
+    RETURN FLOOR(RANDOM() * (high - low + 1) + low);
+END;
+$$ LANGUAGE plpgsql;
