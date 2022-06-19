bb90ee26d3cb3363d84f345f00f44fadfd3a1375
Kathleen Pehl <kathleen.pehl@gmail.com>
2022-06-18 21:56:05 -0400

    node.js structure added with router and api; created models, controllers, routes for api; tested routes with Insomnia Core to verify functionality

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const { Pool, types } = require('pg');
+
+const pool = new Pool();
+
+// return a float from datatype 1700 returned from Postgres, NUMERIC type
+types.setTypeParser(1700, (val) => {
+  return parseFloat(val);
+});
+
+// return an integer from datatype 20 returned from Postgres, from COUNT() functions
+types.setTypeParser(20, (val) => {
+  return parseInt(val);
+});
+
+module.exports = {
+  query: (text, params) => {
+    return pool.query(text, params);
+  }
+};
